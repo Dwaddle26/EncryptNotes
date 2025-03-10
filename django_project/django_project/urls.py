2ab011188a7d0961145109e842f95683dd85c513
Dwaddle26 <dworkmail26@gmail.com>
2025-03-10 12:56:04 -0500

    Fix that updates it back
    Last time I had a huge update committed it. at least I thought, and saw that it broke the app. then reverted it, and lose all progress. So, I making sure this fine and correct and ACTUALLY committing it this time.

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
-    # Path for the Django admin interface.
-    # 'admin/' is the URL path for the admin site.
-    # admin.site.urls includes all the URLs defined for the admin interface.
     path('admin/', admin.site.urls),
-
-    # Include the URL patterns from the 'EncryptNotes' app.
-    # '' (empty string) represents the root URL.
-    # include('EncryptNotes.urls') includes all the URLs defined in the 'urls.py' file of the 'EncryptNotes' app.
-    # This allows you to modularize your URLs by defining them in separate app-level 'urls.py' files.
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('EncryptNotes.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
