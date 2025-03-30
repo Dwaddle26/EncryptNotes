c4293e11dc77fffcc4210a4bcf855e5b6fbc7731
Jacob N <jn251636@my.stchas.edu>
2025-03-30 13:07:31 -0500

    added additional blank templates

--- django_project/EncryptNotes/views.py
+++ django_project/EncryptNotes/views.py
@@ -15,19 +15,28 @@ def about(request):
     return render(request, 'EncryptNotes/about.html', {'title': 'About'})
 
 def create(request):
-    return render(request, 'EncryptNotes/create.html', {'title': 'create'})
+    return render(request, 'EncryptNotes/create.html', {'title': 'Create'})
     
 def announcements(request):
     return render(request, 'EncryptNotes/announcements.html', {'title': 'announcements'})
 
-# Decrypts and lists notes for a user
-def list_user_notes(request):
+# Decrypts and lists notes for a logged in user
+def list(request):
     user = request.user  
     notes = Note.objects.filter(user=user)  
     enc_user_key = user.userprofile.encryption_key
     user_key = decrypt_user_key(enc_user_key)
     decrypt_note_titles = [decrypt_data(user_key, note.title) for note in notes]
     decrypted_note_content = [decrypt_data(user_key, note.content) for note in notes]
-    return render(request, 'notes/listNotes.html', {'title': 'Note list'})
+    return render(request, 'EncryptNotes/list.html', {'title': 'Note list'})
+    
+# View a specific note
+def view(request)
+    user = request.user  
+    notes = Note.objects.filter(user=user)  
+    enc_user_key = user.userprofile.encryption_key
+    user_key = decrypt_user_key(enc_user_key)
+    return render(request, 'EncryptNotes/view.html', {'title:': {note.title}}
+# Edit a specific note
 
 
