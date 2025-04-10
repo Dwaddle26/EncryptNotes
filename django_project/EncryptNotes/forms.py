7f0b4e1a69b47a305aa7bc8afd3b298708ae46ff
Jacob N <jn251636@my.stchas.edu>
2025-04-10 09:31:35 -0500

    Update urls, update templates
    Fix multiple typos and bugs.

--- django_project/EncryptNotes/forms.py
+++ django_project/EncryptNotes/forms.py
@@ -4,7 +4,7 @@ from EncryptNotes.models import Note
 
 # Form for a note
 class NoteForm(forms.ModelForm):
-    itle = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter a title'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter a title'}))
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 40, 'rows': 30}))
     class Meta:
         model = Note
--- django_project/EncryptNotes/urls.py
+++ django_project/EncryptNotes/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('about/', views.about, name='EncryptNotes-about'),
     path('create/', views.create, name='EncryptNotes-create'),
     path('announcements/', views.announcements, name='EncryptNotes-announcements'),
+    path('list/', views.create, name='EncryptNotes-list'),
+    path('edit/', views.create, name='EncryptNotes-edit'),
+    path('view/', views.create, name='EncryptNotes-view')
+    
     
     
 ]
